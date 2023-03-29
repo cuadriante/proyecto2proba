52d1fe06ff3f44758c32eba4d1702805f019fc33
adri <62965634+cuadriante@users.noreply.github.com>
2023-03-28 21:38:41 -0600

    definicion de distribucion a utilizar

--- moneda_ideal.py
+++ moneda_ideal.py
@@ -3,4 +3,8 @@
 # probabilidad es igual para ambas
 # E = 0.5, C = 0.5
 # N = 10
+# 1. Se utiliza la distribución geométrica ya que
+#    X = número de ensayos para un éxito
+#    ensayo = veces que se debe tirar la moneda
+#    éxito = obtener corona
 
--- moneda_truco.py
+++ moneda_truco.py
@@ -3,3 +3,8 @@
 # probabilidad NO es igual para ambas
 # E = 0.7, C = 0.3
 # N = 10
+# 1. Se utiliza la distribución binomial ya que
+#    X = número de éxitos en n ensayos
+#    ensayo = tirar la moneda
+#    éxito = obtener corona
+#    n = 10
