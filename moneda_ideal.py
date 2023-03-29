67c0adf7077a36d154cab2af2804859fe5bddd79
adri <62965634+cuadriante@users.noreply.github.com>
2023-03-28 18:57:19 -0600

    Initial commit

---
+++ moneda_ideal.py
@@ -0,0 +1,6 @@
+# espacio muestral: {escudo, corona}
+# cada tirada dentro de la secuencia es independiente
+# probabilidad es igual para ambas
+# E = 0.5, C = 0.5
+# N = 10
+
---
+++ moneda_truco.py
@@ -0,0 +1,5 @@
+# espacio muestral: {escudo, corona}
+# cada tirada dentro de la secuencia es independiente
+# probabilidad NO es igual para ambas
+# E = 0.7, C = 0.3
+# N = 10
