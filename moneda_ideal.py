9b7a5c9217dd6c99978c184bd3aa38ca69fd76a1
adri <62965634+cuadriante@users.noreply.github.com>
2023-03-29 00:14:59 -0600

    pmf

--- moneda_ideal.py
+++ moneda_ideal.py
@@ -8,3 +8,39 @@
 #    ensayo = veces que se debe tirar la moneda
 #    éxito = obtener corona
 
+import numpy as np
+
+# probabilidad de éxito (Obtener corona)
+p = 0.5
+
+
+def moneda_ideal():
+    # parámetros de la distribución
+    # número de ensayos (tiros de moneda):
+    n = 10
+    # probabilidad de éxito (obtener corona):
+    p = 0.7
+
+    # se utiliza la función np.random.binomial de numpy, la cual
+    # genera una secuencia de 1000 numeros que pertenecen a la distribución binomial
+
+    # función: np.random.binomial
+    # entrada: número de ensayos, probabilidad de éxito, cantidad de números generados
+    # salida: array de los números obtenidos
+    samples = np.random.geometric(p, size=1000)
+
+
+    # función: np.bincount
+    # entrada: números resultados de la distribución binomial
+    # salida: numero de ocurrencias de cada valor en la secuencia
+    ocurrencias = np.bincount(samples, minlength=n+1)
+
+    # Normaliza las ocurrencias para obtener la funcion masa probablidad
+    pmf = ocurrencias / len(samples)
+
+    # PMF:
+    print("PMF de la distribución binomial para la moneda truco con n={}, p={}: \n{}".format(n, p, pmf))
+    print(pmf)
+
+
+moneda_ideal()
--- moneda_truco.py
+++ moneda_truco.py
@@ -8,3 +8,35 @@
 #    ensayo = tirar la moneda
 #    éxito = obtener corona
 #    n = 10
+
+import numpy as np
+
+def moneda_truco():
+    # parámetros de la distribución
+    # número de ensayos (tiros de moneda):
+    n = 10
+    # probabilidad de éxito (obtener corona):
+    p = 0.7
+
+    # se utiliza la función np.random.binomial de numpy, la cual
+    # genera una secuencia de 1000 numeros que pertenecen a la distribución binomial
+
+    # función: np.random.binomial
+    # entrada: número de ensayos, probabilidad de éxito, cantidad de números generados
+    # salida: array de los números obtenidos
+    samples = np.random.binomial(n, p, size=1000)
+
+
+    # función: np.bincount
+    # entrada: números resultados de la distribución binomial
+    # salida: numero de ocurrencias de cada valor en la secuencia
+    ocurrencias = np.bincount(samples, minlength=n+1)
+
+    # Normaliza las ocurrencias para obtener la funcion masa probablidad
+    pmf = ocurrencias / len(samples)
+
+    # PMF:
+    print("PMF de la distribución binomial para la moneda truco con n={}, p={}: \n{}".format(n, p, pmf))
+    print(pmf)
+
+moneda_truco()
